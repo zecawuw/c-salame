3e4c97fa9b2fef697d3f55e6d6520009b4d63ae4
zecawuw <88804350+zecawuw@users.noreply.github.com>
2022-09-02 19:50:52 -0300

    Update imc.c

--- imc.c
+++ imc.c
@@ -17,13 +17,13 @@ int main()
 	
 	imc = peso/(altura*altura);
 	
-	if(imc <= 18,4){
+	if(imc <= 18.4){
 		printf("abaixo do peso \n");
 	}else if(imc >= 18.5, imc <= 24.9){
 		printf("imc peso normal \n");
-    }else if(imc >=25, imc <= 29.9){
+    }else if(imc >=25 , imc <= 29.9){
     	printf("imc sobre peso \n");
-	}else if(imc >= 30, <= 39.9){
+	}else if(imc >= 30 , imc <= 39.9){
 		printf("obesidade \n");
 	}else{
 		printf("obesidade grave \n");
