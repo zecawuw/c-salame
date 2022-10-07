6183d7c330d779c6d4774f4778e778532fd8f881
zecawuw <88804350+zecawuw@users.noreply.github.com>
2022-10-07 15:07:36 -0300

    Add files via upload

---
+++ 10_e_20_mostrar_mesmo.c
@@ -0,0 +1,20 @@
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
+    int z, vet[10], t = 10;
+    
+    for(z=0;z<11;z++){
+        vet[z]=t;
+        t++;
+    }
+    printf("\n\n");
+    
+    for(z=0;z<12;z++){
+        printf("\n Os numeros do vetor são: %d",vet[z]);
+    }
+    
+    system("pause");
+    return 0;
+}
